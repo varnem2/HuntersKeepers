97346a48e8774a6b693f3960091292de90ce7852
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2020-03-15 12:52:28 -0400

    Improve UX/UI for Fate

--- app/helpers/fates_helper.rb
+++ app/helpers/fates_helper.rb
@@ -1,2 +1,32 @@
+# frozen_string_literal: true
+
 module FatesHelper
+  def tag_select(form:, field:)
+    tag.div class: 'select' do
+      form.select(
+        field,
+        collection_for(field: field),
+        multiple: true
+      )
+    end
+  end
+
+  def collection_for(field:)
+    case field
+    when :found_out_list
+      Fate::FOUND_OUT
+    end
+  end
+
+  def fate_buttons(fate)
+    tag.div class: 'buttons has-addons' do
+      concat link_to 'Show', fate, class: 'button is-small'
+      concat link_to 'Edit', edit_fate_path(fate), class: 'button is-small'
+      concat link_to 'Destroy',
+                     fate,
+                     method: :delete,
+                     data: { confirm: 'Are you sure?' },
+                     class: 'button is-small'
+    end
+  end
 end
--- app/helpers/hunters_helper.rb
+++ app/helpers/hunters_helper.rb
@@ -4,7 +4,11 @@
 module HuntersHelper
   def hunter_select(form)
     tag.div class: 'select' do
-      form.collection_select :playbook_id, Hunter.all, :id, :name
+      form.collection_select :hunter_id, Hunter.all, :id, :name
     end
+  end
+
+  def hunter_link(hunter)
+    link_to hunter.name, hunter
   end
 end
